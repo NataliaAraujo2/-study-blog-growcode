3c46717610e653e435351735d47d90867997e990
NataliaAraujo2 <nataliaanog@gmail.com>
2022-11-24 19:50:30 -0300

    Button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles.css';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [theme, setTheme] = useState('day');
 
   useEffect(() => fetchPosts(), []);
 
@@ -19,16 +20,24 @@ function App() {
       });
   }
 
+  function handleTheme() {
+    setTheme((theme) => (theme === 'night' ? 'day' : 'night'))
+  }
+
   return (
-    <div className='main-container'>
+    <div className='main-container' data-theme={theme}>
       <header>
         <h1>BLOG LUNA</h1>
-        <button>New Post</button>
-        <button>Dark Mode</button>
+        <button className='btn primary'>New Post</button>
+        <button className='btn primary' onClick={() => handleTheme()}>
+          {theme === 'night' ? 'Day' : 'Night'} Mode</button>
       </header>
       <main>
         {posts.map((post) => (
-          <section className="post-card" id={post.id}>
+          <section
+            className="post-card"
+            id={post.id}
+            onClick={(e) => console.log(e)}>
             <div className='post-preview-content'>
               <div className='post-author'>
                 <img
@@ -47,12 +56,16 @@ function App() {
               <img
                 src={`https://picsum.photos/id/${10 * post.id}/100`}
                 alt='imagem do post'
-
               />
             </div>
           </section>
         ))}
       </main>
+      <section className='fullscreen-post'>
+      <button className='btn secundary' >Close</button>
+        <h1>Título</h1>
+        <p>Conteúdo do Post</p>
+      </section>
     </div>
   )
 }
